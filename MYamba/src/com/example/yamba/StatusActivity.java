f5159f10fb27aaac08268fb168c679775ed00473
Marko Gargenta <marko@marakana.com>
2012-03-05 17:31:52 -0800

    Implemented basic Status Activity, with Log.d()

--- MYamba/src/com/example/yamba/StatusActivity.java
+++ MYamba/src/com/example/yamba/StatusActivity.java
@@ -2,12 +2,30 @@ package com.example.yamba;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class StatusActivity extends Activity {
+public class StatusActivity extends Activity implements OnClickListener {
+	EditText editStatus;
+	Button buttonUpdate;
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.status);
+        
+        editStatus = (EditText) findViewById(R.id.edit_status);
+        buttonUpdate = (Button) findViewById(R.id.button_update);
+        buttonUpdate.setOnClickListener(this);
     }
+
+	@Override
+	public void onClick(View view) {
+		String status = editStatus.getText().toString();
+		Log.d("StatusActivity", "onClicked with status: "+status);
+	}
 }
