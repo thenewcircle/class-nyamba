43b014d2d2fa4102b40ab2f9301d728ca89fb3e7
Marko Gargenta <marko@marakana.com>
2012-03-06 16:24:35 -0800

    Implemented AboutActivity

--- MYamba/src/com/example/yamba/AboutActivity.java
+++ MYamba/src/com/example/yamba/AboutActivity.java
@@ -2,13 +2,26 @@ package com.example.yamba;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class AboutActivity extends Activity {
+	WebView webview;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.about);
-	}
 
+		webview = (WebView) findViewById(R.id.webview);
+		webview.setWebViewClient( new WebViewClient() {
+			@Override
+			public boolean shouldOverrideUrlLoading(WebView view, String url) {
+				view.loadUrl(url);
+				return true;
+			} 
+		});
+
+		webview.loadUrl("file:///android_asset/index.html");
+	}
 }
